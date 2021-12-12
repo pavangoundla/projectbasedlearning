bbe878835e67a7ddf925df7146bcb0924d392717
pavangoundla <pavangoundla0707@gmail.com>
2021-12-12 22:46:07 +0530

    masterfirstommit

---
+++ master/masterproject/src/masterproject/MasterClass.java
@@ -0,0 +1,10 @@
+package masterproject;
+
+public class MasterClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I'm a master");
+	}
+
+}
